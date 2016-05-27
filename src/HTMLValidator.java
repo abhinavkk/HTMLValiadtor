91b8f346d066e3e4a94879351b7efade6f334ff8
abhinavkk <keshariabhinavk1996@gmail.com>
2016-05-27 11:04:38 +0530

    Adding Validation class

---
+++ src/HTMLValidator.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class HTMLValidator {
+	private static String HTML;
+    
+    public static void main(String[] args) throws Exception {
+    	System.out.println("Enter:");
+    	Scanner input = new Scanner(System.in);
+    	HTML = input.nextLine();
+        parseWithJsoup();
+    }
+    private static void parseWithJsoup() {
+        Document doc = Jsoup.parseBodyFragment(HTML);
+        doc.outputSettings().prettyPrint(false);
+        
+        String parsedhtml = doc.body().html();
+        System.out.println("Output HTML String:");
+        System.out.printf("%s\n\n", parsedhtml);
+        
+        if(HTML.equals(parsedhtml))
+        	System.out.println("Inputted html is correct");
+        else
+        {
+        	System.out.println("Inputted html is not correct");
+        }
+    	System.out.println("\nHTML Format:");
+    	doc.outputSettings().prettyPrint(true);
+    	System.out.printf("%s\n", doc.body());
+        System.out.println();
+    }
+}
